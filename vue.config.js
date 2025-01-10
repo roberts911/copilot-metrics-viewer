ee8e10b2bb67b5ecf2aebbb645678f0cd3b37eba
Robert Siurek <robertspabianice23@gmail.com>
2025-01-10 23:53:38 +0100

    test

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,14 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
 
   pluginOptions: {
     vuetify: {
-			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
-		}
-  }
-})
+      // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+    },
+  },
+
+  // Dodanie publicPath dla poprawnej obsługi ścieżek na GitHub Pages
+  publicPath: '/copilot-metrics-viewer/', // Zastąp <repo_name> nazwą swojego repozytorium
+});
+
